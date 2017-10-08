3ffb2792bddedfe8a3cbf5c852995e34cae20e4f
davidjaimesdesign@gmail.com <davidjaimesdesign@gmail.com>
2017-10-08 16:10:07 -0700

    ravioli ravioli show me the formuoli

--- sopy.rb
+++ sopy.rb
@@ -8,7 +8,7 @@ class Sopy < Formula
   version "1.0.0"
 
   url "https://github.com/DavidJaimesDesign/homebrew-sopy/archive/0.2.tar.gz"
-  version "1.0.0"
+  version "0.2.0"
   #sha256 "4cf2e80427ac4ad3581ce62e722eb17cebd6405f81095755bde2bdd88c26fc65"
 
   depends_on :python
