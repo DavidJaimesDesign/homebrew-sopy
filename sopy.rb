e002e3c28c4971d4fb6f4ddde44b190d578670c7
davidjaimesdesign@gmail.com <davidjaimesdesign@gmail.com>
2017-10-07 23:06:14 -0700

    ravioli ravioli show me the formuoli

--- sopy.rb
+++ sopy.rb
@@ -22,7 +22,7 @@ class Sopy < Formula
   #system "python", *Language::Python.setup_install_args(prefix)
 
   def install
-    virtualenv_install_with_resources 
+    #virtualenv_install_with_resources 
 	  # ENV.deparallelize  # if your formula fails when building in parallel
 
     # Remove unrecognized options if warned by configure
